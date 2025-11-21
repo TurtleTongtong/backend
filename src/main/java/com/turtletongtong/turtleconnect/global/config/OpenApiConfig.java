24679c5c44804dce7e6895b2387b5cd4d1f4ffd1
youngjin516 <124585696+youngjin516@users.noreply.github.com>
2025-11-21 16:37:46 +0900

    fix/#3
    스웨거 파라미터 헤더칸 삭제 및 상단 Authorize 버튼 통일

--- src/main/java/com/turtletongtong/turtleconnect/global/config/OpenApiConfig.java
+++ src/main/java/com/turtletongtong/turtleconnect/global/config/OpenApiConfig.java
@@ -17,24 +17,18 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        String jwtScheme = "JWT Token";
-
-        SecurityRequirement securityRequirement =
-                new SecurityRequirement().addList(jwtScheme);
-
-        SecurityScheme securityScheme =
-                new SecurityScheme()
-                        .name(jwtScheme)
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT");
-
         return new OpenAPI()
                 .info(new Info()
                         .title("TurtleConnect API")
                         .description("거북섬 프로젝트 API 문서")
                         .version("1.0"))
-                .addSecurityItem(securityRequirement)
-                .components(new Components().addSecuritySchemes(jwtScheme, securityScheme));
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
+
 }
